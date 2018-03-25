a43ea87e522f6076c9330c2a4ea8307201f5446a
root <root@centos-512mb-sgp1-01.(none)>
2018-03-26 03:47:28 +0900

    init

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <title>好味道</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,400italic,600,700|Montserrat:400,700|Merriweather" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <link rel="stylesheet" href="css/dark.css" type="text/css" />
+        <link rel="stylesheet" href="css/app-landing.css" type="text/css" />
+    </head>
+    <body class="stretched device-lg">
+        <div id="wrapper" class="clearfix" style="animation-duration: 1.5s; opacity: 1;">
+            <header id="header" class="split-menu transparent-header" data-sticky-class="not-dark" data-responsive-class="not-dark">
+                <div id="header-wrap">
+                    <div class="container clearfix">
+                        <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+                        <div id="logo">
+                        </div>
+                        <nav id="primary-menu" class="with-arrows clearfix not-dark">
+                            <ul class="one-page-menu" data-easing="easeInOutExpo" data-speed="1250" data-offset="160">
+                                <li><a href="#" data-href="#wrapper"><div>TOP</div></a></li>
+                                <li><a href="#" data-href="#section-nextgen"><div>SERVICE</div></a></li>
+                                <li><a href="#" data-href="#section-pricing"><div>DOWNLOAD</div></a></li>
+                            </ul>
+
+                            <ul class="one-page-menu" data-easing="easeInOutExpo" data-speed="1250" data-offset="160">
+                                <li><a href="#" data-href="#section-faqs"><div>FAQs</div></a></li>
+                                <li><a href="#" data-href="#section-gallery"><div>規約</div></a></li>
+                                <li class="menu-item-emphasis"><a href="/" ><div>ONLINE SHOP</div></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+
+
+            <section id="slider" class="full-screen slider-parallax slider-parallax-visible">
+                <div class="slider-parallax-inner" style="background: url('images/bg.png') center center no-repeat; background-size: cover;">
+                    <div class="vertical-middle" style="z-index: 2; position: absolute; top: 50%; width: 100%; padding-top: 0px; padding-bottom: 0px; margin-top: -90px; opacity: 1.19494;">
+                        <div class="container dark clearfix">
+                            <div class="row clearfix">
+                                <div class="col-md-6 col-sm-8">
+                                    <div class="emphasis-title">
+                                    <h1 class="font-body"> 好 味 道 ，<br> ? 美 食 来 敲 ? 。</h1>
+                                    </div>
+
+                                    <div>
+                                        <a href="#" data-scrollto="#section-pricing" data-easing="easeInOutExpo" data-speed="1250" data-offset="160" class=" button button-large button-white button-light" style="border-radius: 23px; text-transform: none;">ONLINE SHOP</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="video-wrap" style="position: absolute; height: 100%; z-index: 1;">
+                        <div class="video-overlay" style="background: rgba(0,0,0,0.2);"></div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
+
